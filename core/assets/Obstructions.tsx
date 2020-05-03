4c2460f286899e7c09f2bd7c6d8ba1151283ab6f
Francis Brazeau <hatchet2k4@gmail.com>
2020-05-02 20:40:33 -0400

    obstruction daataa

---
+++ core/assets/Obstructions.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="Obstructions" tilewidth="16" tileheight="16" spacing="1" margin="1" tilecount="40" columns="10">
+ <image source="obstructions.png" trans="ff00ff" width="171" height="69"/>
+</tileset>
